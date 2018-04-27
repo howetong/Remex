b3cda49931d2afbece312453f45ba731d91adbe2
howetong <howetong@blackfish.cn>
2018-04-27 19:58:41 +0800

    build foundation

--- remex-common/src/main/java/cn/tonghao/remex/common/util/ParameterUtil.java
+++ remex-common/src/main/java/cn/tonghao/remex/common/util/ParameterUtil.java
@@ -1,5 +1,12 @@
 package cn.tonghao.remex.common.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,10 +17,12 @@ import java.util.Map;
  */
 public class ParameterUtil {
 
+    public static final Logger logger = LoggerFactory.getLogger(ParameterUtil.class);
+
     /**
      * 生成签名源串，忽略掉Null的值。
-     * @param params
-     * @return
+     * @param params 参数map
+     * @return String 排序的参数串
      */
     public static String getSignDataIgnoreNull(Map<String, String> params) {
         StringBuffer content = new StringBuffer();
@@ -34,5 +43,34 @@ public class ParameterUtil {
             }
         }
         return content.toString();
+    }
+
+    public static String getJsonRequestData(HttpServletRequest request, HttpServletResponse response) {
+        if (null == request) {
+            return null;
+        }
+        String method = request.getMethod();
+        String ret = null;
+        if (method.equalsIgnoreCase("GET") || method.equalsIgnoreCase("DELETE")) {
+            ret = request.getQueryString();
+        } else {
+            ret = getBodyData(request);//获取请求body
+        }
+        return ret;
+    }
+
+    private static String getBodyData(HttpServletRequest request) {
+        StringBuffer data = new StringBuffer();
+        String line;
+        BufferedReader reader;
+        try {
+            reader = request.getReader();
+            while (null != (line = reader.readLine()))
+                data.append(line);
+        } catch (IOException e) {
+            logger.error("get request body data error: ", e);
+            return null;
+        }
+        return data.toString();
     }
 }
